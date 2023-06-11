9a7cf4c844a2278ecec1b2c693402d275b729de5
rx78gp01 <rx78gp01@users.noreply.github.com>
2023-06-11 23:54:10 +0800

    Revert "Update SparseItemUtil.java"
    This reverts commit 62d3edf1c4a7600778730c88d2fbfdef95fe1108.

--- app/src/main/java/org/schabi/newpipe/util/SparseItemUtil.java
+++ app/src/main/java/org/schabi/newpipe/util/SparseItemUtil.java
@@ -101,10 +101,10 @@ public final class SparseItemUtil {
                                                         final int serviceId,
                                                         @NonNull final String url,
                                                         final Consumer<StreamInfo> callback) {
-        //Toast.makeText(context, R.string.loading_stream_details, Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, R.string.loading_stream_details, Toast.LENGTH_SHORT).show();
         ExtractorHelper.getStreamInfo(serviceId, url, false)
                 .subscribeOn(Schedulers.io())
-                .observeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(result -> {
                     // save to database in the background (not on main thread)
                     Completable.fromAction(() -> NewPipeDatabase.getInstance(context)
